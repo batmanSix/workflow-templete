7dde89296a5424c65d824c2c594f340282cd1e4c
kangbo <841331654@qq.com>
2022-09-01 12:24:40 +0800

    feat(Vue): 修改提交脚本

--- .cz-config.js
+++ .cz-config.js
@@ -1,15 +1,19 @@
 'use strict';
 module.exports = {
   types: [
-    { value: '新增', name: '新增:    新的内容' },
-    { value: '修复', name: '修复:    修复一个Bug' },
-    { value: '文档', name: '文档:    变更的只有文档' },
-    { value: '格式', name: '格式:    空格, 分号等格式修复' },
-    { value: '重构', name: '重构:    代码重构，注意和特性、修复区分开' },
-    { value: '性能', name: '性能:    提升性能' },
-    { value: '测试', name: '测试:    添加一个测试' },
-    { value: '工具', name: '工具:    开发工具变动(构建、脚手架工具等)' },
-    { value: '回滚', name: '回滚:    代码回退' }
+    { value: 'feat', name: 'feat:     新功能' },
+    { value: 'fix', name: 'fix:      修复' },
+    { value: 'docs', name: 'docs:     文档变更' },
+    { value: 'style', name: 'style:    代码格式(不影响代码运行的变动)' },
+    {
+      value: 'refactor',
+      name: 'refactor: 重构(既不是增加feature，也不是修复bug)'
+    },
+    { value: 'perf', name: 'perf:     性能优化' },
+    { value: 'test', name: 'test:     增加测试' },
+    { value: 'chore', name: 'chore:    构建过程或辅助工具的变动' },
+    { value: 'revert', name: 'revert:   回退' },
+    { value: 'build', name: 'build:    打包' }
   ],
   scopes: [
     { name: 'leetcode' },
